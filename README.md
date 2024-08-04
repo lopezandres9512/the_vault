eb3cb48c81ee263edb32d1fa3fb9c22f25e719a8
Fabian Lopez <lombareload@gmail.com>
2024-08-04 04:50:11 -0500

    add spring boot project to application

---
+++ README.md
@@ -0,0 +1,14 @@
+# Hotel Java app
+
+## Requirements
+- Java 22
+- Docker compose
+
+## Execution
+The app can be executed by running `./mvnw spring-boot:run`
+if executed successfully a simple controller should be reachable
+at http://localhost:8080/api/reservation-controller 
+
+## Next steps
+- [ ] Add spring properties to connect to mysql docker-compose container
+- [ ] Automate creation of required tables
---
+++ src/main/java/com/hotel/app/reservations/controllers/ReservationController.java
@@ -0,0 +1,14 @@
+package com.hotel.app.reservations.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/reservation-controller")
+public class ReservationController {
+    @GetMapping
+    public String get() {
+        return "works!";
+    }
+}
